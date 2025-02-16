e4898bbab88ad0b69107b8d59da4cb9ae849f391
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-02-17 00:54:31 +0330

    fix: make sample ready

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,13 +1,11 @@
-import { ASSETS } from '@/assets';
 import { GridContainer } from '@/components/core/GridContainer';
-import { Image } from '@/components/core/Image';
 
 export function Header() {
   return (
-    <GridContainer justifyContent='justify-between' className='max-h-[40] px-2' backgroundColor='bg-neutral-pure-black'>
-      <Image src={ASSETS.arrowRight} width={24} height={32} />
-      <Image src={ASSETS.logo} className='scale-150' width={40} height={40} />
-      <Image src={ASSETS.profile} width={24} height={24} />
-    </GridContainer>
+    <GridContainer
+      justifyContent='justify-between'
+      className='max-h-[40] px-2'
+      backgroundColor='bg-neutral-pure-black'
+    ></GridContainer>
   );
 }
