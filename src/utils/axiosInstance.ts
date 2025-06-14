009e77ceda5e623088a9a3f3cb3857b058bbe0a4
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-06-14 19:37:44 +0330

    feat: add auth token in header - check ssr util - add new urls

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -10,6 +10,15 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json'
   }
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('auth_token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
