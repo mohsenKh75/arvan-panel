e59e105c7b1b85c1c28fa87a1f250453ae021fc6
Mohsen Khademhosseini <mohsenpm77@gmail.com>
2025-06-22 20:09:12 +0330

    feat: add layout props to the page

--- src/components/Layout/MainLayoutContainer.tsx
+++ src/components/Layout/MainLayoutContainer.tsx
@@ -1,15 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatches } from 'react-router-dom';
 import { Box } from '@/components/core/Box';
 import { GridContainer } from '@/components/core/GridContainer';
+import { classnames } from '@/utils/classnames';
+import { LayoutProps } from './layoutProps';
 
 export function MainLayoutContainer({ children }: { children: React.ReactNode }) {
+  const matches = useMatches();
+  const layoutProps = matches?.[matches.length - 1]?.handle as {
+    layoutProps: LayoutProps;
+  };
+
+  const isCentered = layoutProps.layoutProps.position === 'centered';
+
   return (
     <GridContainer direction='flex-col' className='mx-auto min-h-screen h-full bg-gray-300'>
-      <Box tag='header'></Box>
-      <Box tag='main' className='flex-1 flex items-center justify-center'>
+      <Box
+        tag='main'
+        className={classnames('flex-1 flex', {
+          'items-center justify-center': isCentered
+        })}
+      >
         {children}
       </Box>
-      <Box tag='footer' className='p-4 text-center'></Box>
     </GridContainer>
   );
 }
